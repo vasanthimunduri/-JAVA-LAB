6f602f52b50f6bcdabc8001a65a34e4953b8b0fb
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-07-27 16:29:30 +0530

    Create DEFAULTVALUES.java

---
+++ DEFAULTVALUES.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Default
+{
+static byte b;
+static boolean bool;
+static char ch;
+static short s;
+static int i;
+static float f;
+static long l;
+static double d;
+public static void main (String args[])
+{
+System.out.println(b);
+System.out.println(bool);
+System.out.println(ch);
+System.out.println(s);
+System.out.println(i);
+System.out.println(f);
+System.out.println(l);
+System.out.println(d);
+}
+}
