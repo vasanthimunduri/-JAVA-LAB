2580d41502ec31d33f095e23ae7c439bd99c33ee
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-08-08 12:09:32 +0530

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class For
+{
+public static void main(String args[])
+{
+int arr[]={10,11,12,13};
+System.out.println("The Elements are: ");
+for(int i:arr){
+System.out.println(i);
+}
+}
+}
