6ffbf92f723486681e5842fe3d4d9dbd843ef32d
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-08-08 12:04:52 +0530

    Create Arguments.java

---
+++ Arguments.java
@@ -0,0 +1,25 @@
+//printing first and second arguments
+
+import java.util.*;
+class CLA
+{
+public static void main(String args[])
+{
+System.out.println("The First Argument is : "+args[0]);
+System.out.println("The second Argument is : "+args[1]);
+}
+}
+
+
+//printing all given arguments
+import java.util.*;
+class CLA
+{
+public static void main(String args[])
+{
+System.out.println("The Arguments are: ");
+for(int i=0;i<args.length;i++){
+System.out.println(args[i]);
+}
+}
+}
