0ba160370a8f8c82ee378b53693c3b4a43272c17
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-08-08 12:15:36 +0530

    Update ForEach.java

--- ForEach.java
+++ ForEach.java
@@ -1,3 +1,4 @@
+//printing elements
 import java.util.*;
 class For
 {
@@ -8,5 +9,23 @@ System.out.println("The Elements are: ");
 for(int i:arr){
 System.out.println(i);
 }
+}
+}
+
+
+//printing total of elements 
+import java.util.*;
+class For
+{
+public static void main(String args[])
+{
+int arr[]={10,11,12,13};
+int total=0;
+System.out.println("The Elements are: ");
+for(int i:arr){
+System.out.println(i);
+ total += i;
+}
+System.out.println("The total is: "+total);
 }
 }
