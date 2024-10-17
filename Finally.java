8505fe9b5c29976b9903013f0121f884de544b3b
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-10-17 11:38:08 +0530

    Update Finally.java

--- Finally.java
+++ Finally.java
@@ -1,3 +1,4 @@
+//If Exception Did not Handled
 import java.util.*;
 class MultiCatch
 {
@@ -19,4 +20,31 @@ System.out.println("Hello People");
 
 //the statement in finally block printed even though the exception not handled by the catch block
 //Output-Hello people
-         Arithmetic Exception
+//       ArrayIndexOfBoundsException
+
+
+//If exception Handled
+import java.util.*;
+class MultiCatch
+{
+public static void main(String args[])
+{
+try {
+int a[]=new int[5];
+System.out.println(a[10]);
+}
+catch(ArithmeticException ae)
+{
+System.out.println("Arithmetic Exception");
+}
+catch(ArrayIndexOutOfBoundsException ai){
+System.out.println("Out Of bounds Exception");
+}
+finally{
+System.out.println("Hello People");
+}
+}
+}
+//Output-Out of Bounds Exception
+//       Hello People
+
